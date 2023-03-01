1bf55549bba39b7977236fd2bc2f5c22bcd7316c
Ramón López <63748740+Ramonst18@users.noreply.github.com>
2023-02-28 21:05:09 -0700

    Proceso

---
+++ Funciones/ejercicio7.cpp
@@ -0,0 +1,12 @@
+/*7. Intercambiar el valor de 2 variables utilizando el paso de parametros
+por referencia.*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    //variables a utilizar
+    int num1 = 10, num2 = 15;
+
+    return 0;
+}
